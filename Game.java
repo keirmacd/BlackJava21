bf1b215dd189f3e90be324a224796cf20dcdd42f
Keir Macdonald <keirmacd@hotmail.co.uk>
2017-02-19 19:30:04 +0000

    My syntax sucks

--- Game.java
+++ Game.java
@@ -1,11 +1,15 @@
 public class Game{
  private Player player1;
  private Player player2;
-}
 
 public Player1{
 hand = player1.hand
 }
 public Player2{
   hand = player2.hand
+}
+
+public Iswon{
+player1.hand >= player2.hand
+}
 }
